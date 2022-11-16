9d36c4957c5c8ea94a5f1f8cd08a19caff79b571
Raphaël Titsworth-Morin <raphael@raphaeltm.com>
2022-11-15 22:30:53 -0800

    setting up tooling to easily push across all repos

---
+++ contributor-cli/src/commands/version/push.ts
@@ -0,0 +1,64 @@
+import {Command, Flags} from '@oclif/core'
+// eslint-disable-next-line unicorn/import-style
+import * as chalk from 'chalk'
+import {GitProcess} from 'dugite'
+import {findRoot} from '../../lib/context/find-root'
+import {findSubmodulePaths} from '../../lib/context/find-submodule-paths'
+
+interface PushOptions {
+  setUpstream: boolean;
+  branch: string;
+}
+
+const push = async (path: string, {setUpstream, branch}: PushOptions) => {
+  console.log('@@ About to push', path)
+  const result = await (setUpstream ?
+    GitProcess.exec(['push', '--set-upstream', 'origin', branch], path) :
+    GitProcess.exec(['push'], path))
+
+  if (result.exitCode !== 0) {
+    throw new Error(result.stderr)
+  }
+
+  const root = await findRoot()
+  const dirname = root === path ? '/' : path.replace(root, '')
+
+  console.log(`✅ Successfully pushed ${chalk.bold.green(dirname)}`)
+}
+
+export default class Switch extends Command {
+  static description =
+    'Push work on a given version (assumes everything has been properly committed)';
+
+  static examples = [
+    `
+chewy-cc version push --set-upstream
+`,
+  ];
+
+  static flags = {
+    setUpstream: Flags.boolean(),
+  };
+
+  static args = [];
+
+  async run(): Promise<void> {
+    const parsed = await this.parse(Switch)
+    const {
+      flags: {setUpstream = false},
+    } = parsed
+
+    const rootPath = await findRoot()
+
+    const branch = (
+      await GitProcess.exec(['branch', '--show-current'], rootPath)
+    ).stdout.replace('\n', '')
+
+    const submodules = await findSubmodulePaths()
+
+    await Promise.all(
+      submodules.map(path => push(path, {setUpstream, branch})),
+    )
+    await push(rootPath, {setUpstream, branch})
+  }
+}
