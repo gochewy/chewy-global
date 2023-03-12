fe83732059913fb5d5f0c4ea0d2d057bcbacb948
Raphaël Titsworth-Morin <raphael@raphaeltm.com>
2023-03-12 02:31:49 +0000

    update initialization script

--- .gitpod/initialize.sh
+++ .gitpod/initialize.sh
@@ -1,8 +1,9 @@
  git submodule update --init --recursive
  git submodule update --recursive
-(cd docs && yarn install)
+
 (cd contributor-cli && yarn install && yarn build)
+/workspace/chewy-global/contributor-cli/bin/chewy-cc git checkout main
+
+(cd docs && yarn install)
 (cd cli && yarn install && yarn build)
 (cd lib && yarn install)
-
-/workspace/chewy-global/contributor-cli/bin/chewy-cc git checkout
